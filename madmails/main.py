4ab064540b15eedf81ece299da3510850b6b564a
Julien Klaer <klaer.julien@gmail.com>
2021-09-26 01:42:56 +0200

    feat: simple mail send endpoint with the SendMailRequest model

--- madmails/main.py
+++ madmails/main.py
@@ -5,13 +5,13 @@ It runs a FastAPI webserver with uvicorn.
 import uvicorn
 from fastapi import FastAPI
 
+from controller import mail
+
+# Initialization of a new FastAPI app
 app = FastAPI()
 
-
-@app.get("/api/hello/{name}")
-async def hello(name: str):
-    return {"message": f"hello {name}"}
-
+# Inclusion of new routers. Same as Flask blueprints.
+app.include_router(mail.router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=5000, reload=True, log_level="info")
