b479ed50c6d553c704e21f1d0aaeaf48cb0c9956
Julien Klaer <klaer.julien@gmail.com>
2021-09-26 00:35:35 +0200

    feat: super simple fastapi endpoint to start with

---
+++ madmails/main.py
@@ -0,0 +1,13 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/api/hello/{name}")
+async def hello(name: str):
+    return {"message": f"hello {name}"}
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="127.0.0.1", port=5000, reload=True, log_level="info")
