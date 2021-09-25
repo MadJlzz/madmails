c00141238e5c8236eecac2bf438a910a87e6de68
Julien Klaer <klaer.julien@gmail.com>
2021-09-26 01:05:40 +0200

    style: fixing pylint error linked to quote usage and module documentation

--- madmails/main.py
+++ madmails/main.py
@@ -1,3 +1,7 @@
+"""
+This module is the entrypoint of the project.
+It runs a FastAPI webserver with uvicorn.
+"""
 import uvicorn
 from fastapi import FastAPI
 
@@ -9,5 +13,5 @@ async def hello(name: str):
     return {"message": f"hello {name}"}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=5000, reload=True, log_level="info")
