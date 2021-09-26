ec28353402a624368f5b0635a139ac067de69cdc
Julien Klaer <klaer.julien@gmail.com>
2021-09-26 22:53:39 +0200

    test: mocking properly the service to test the controller layer

--- madmails/main.py
+++ madmails/main.py
@@ -4,11 +4,26 @@ It runs a FastAPI webserver with uvicorn.
 """
 import uvicorn
 from fastapi import FastAPI
+from loguru import logger
+from starlette.requests import Request
+from starlette.responses import Response
 
 from madmails.controller import mail
 
 # Initialization of a new FastAPI app
 app = FastAPI(title="MadMails", description="A developer friendly service for sending mails.", version="0.1.0")
+
+
+# Global exception handler to catch any unexpected exception
+# and send a pretty response.
+@app.middleware("http")
+async def catch_exceptions_middleware(request: Request, call_next):
+    try:
+        return await call_next(request)
+    except Exception as err:  # pylint: disable=broad-except
+        logger.error("something unexpected happened: {}", err)
+        return Response("Internal server error", status_code=500)
+
 
 # Inclusion of new routers. Same as Flask blueprints.
 app.include_router(mail.router)
--- tests/controller/test_mail.py
+++ tests/controller/test_mail.py
@@ -1,4 +1,5 @@
 from fastapi.testclient import TestClient
+from pytest_mock import MockerFixture
 
 from madmails.main import app
 
@@ -12,8 +13,10 @@ def test_send_mail_with_wrong_input_returns_error():
     assert response.json() == {"detail": [{"loc": ["body"], "msg": "field required", "type": "value_error.missing"}]}
 
 
-def test_send_mail_delegate_correctly_to_sender_service(mocker):
-    sender_mock = mocker.patch("madmails.service.factory.MailSenderFactory.get_mail_sender")
+def test_send_mail_delegate_correctly_to_sender_service(mocker: MockerFixture):
+    # Important: https://docs.python.org/3/library/unittest.mock.html#where-to-patch
+    factory_mock = mocker.patch("madmails.controller.mail.MailSenderFactory.get_mail_sender", autospec=True)
+
     response = test_client.request(method="POST",
                                    url="/mail/send",
                                    headers={"Content-Type": "application/json"},
@@ -25,11 +28,24 @@ def test_send_mail_delegate_correctly_to_sender_service(mocker):
                                        "footer": "theFooter"
                                    })
     assert response.status_code == 200
-    assert sender_mock.call_count == 1
-    # How do I mock this??
-    # assert sender_mock.send.call_count == 1
+    assert factory_mock.called is True
+    assert factory_mock.return_value.send.call_count == 1
 
 
-def test_send_mail_factory_throws_error():
-    # TODO: write this unit test after reading more about mocking in Python.
-    assert True
+def test_send_mail_factory_throws_error(mocker: MockerFixture):
+    factory_mock = mocker.patch("madmails.controller.mail.MailSenderFactory.get_mail_sender", autospec=True)
+    factory_mock.side_effect = NotImplementedError()
+
+    response = test_client.request(method="POST",
+                                   url="/mail/send",
+                                   headers={"Content-Type": "application/json"},
+                                   json={
+                                       "to_recipient": ["foo@bar.dev"],
+                                       "cc_recipient": ["bar@foo.dev"],
+                                       "subject": "theSubject",
+                                       "body": "theBody",
+                                       "footer": "theFooter"
+                                   })
+
+    assert response.status_code == 500
+    assert response.reason == "Internal Server Error"
